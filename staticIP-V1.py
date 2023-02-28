cf02e0db3fe72843ed71b111aecf0d0ac8068b56
Krishna Kumar <krishnakmr438@gmail.com>
2023-02-28 09:23:48 +0530

    Code working the loop was incorrect
    Code working the loop was incorrect

--- staticIP-V1.py
+++ staticIP-V1.py
@@ -1,54 +1,50 @@
 from netmiko import ConnectHandler
 import os
-import pandas as pd
 import datetime
 import csv
 import re
 import getpass
 
-
-reader = pd.read_csv("Bandhan_IP.csv")
-router_ip = reader["Router_IP"].tolist()
-client_id = reader["Client_ID"].tolist()
-static_ip = reader["Static_IP"].tolist()
+router_ip=[]
+client_id=[]
+static_ip=[]
 
 
-length = len(router_ip)
-print(length)
-router_ip_set = set(router_ip)
-# print((router_ip_set))
-for i in range(length):
-    for ip in router_ip_set:
-        name = ip + ".txt"
-        if router_ip[i] == ip:
-            with open(name ,"w+") as c:
-                c.writelines("")
-            with open(name ,"a+") as c:
-                c.writelines("ip dhcp pool Distribution-Server20\n")
-                c.writelines(static_ip[i]+"\n")
-                c.writelines( client_id[i]+"\n" )
+with open ("Bandhan_IP.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        router_ip.append(row['Router_IP'])
+        client_id.append(row['Client_ID'])
+        static_ip.append(row['Static_IP'])
+    for i in range(len(router_ip)):
+        for ip in router_ip:
+            print(ip)
+            name=(ip + ".txt")
+            if router_ip[i]==ip:
+                with open (name,'a+') as c:
+                    c.writelines(static_ip[i]+'\n')
+                    c.writelines(client_id[i]+'\n')
 
-length = len(router_ip_set)
-#username = input("enter username")
-#password = getpass.getpass()
-for i in range(length):
+
+for ip in router_ip:
     cisco1 = {
-    "device_type":'cisco_ios' ,
-    "host": ip,
-    "username": 'cisco',
-    "password":'cisco' ,
-    "fast_cli": False,
-    "global_delay_factor": 3,
-    }
-    
+        "device_type":'cisco_ios' ,
+        "host": ip,
+        "username": 'cisco',
+        "password":'cisco' ,
+        "fast_cli": False,
+        "global_delay_factor": 3,
+        }
+        
     net_connect = ConnectHandler(**cisco1)
     net_connect.enable()
     name = ip + ".txt"
     command=net_connect.send_config_from_file(name)
-
+    print(command)
+    net_connect.save_config()
     confirmation=net_connect.send_command('sh run | sec Distribution-Server20')
     #finding version in output using regular expressions
-    pattern=("ip dhcp pool Distribution-Server20")
+    pattern=("ip dhcp pool Distribution-Server 20")
     match = re.search(pattern,confirmation)
     if match:
         with open ('Final_report.txt',"a+") as f:
@@ -62,11 +58,6 @@ for i in range(length):
             f.writelines("\n")
             f.close()
 
-        
 
-        
-        
-
-
-
-
+            
+    
